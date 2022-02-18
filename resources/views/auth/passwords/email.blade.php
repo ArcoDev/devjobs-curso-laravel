7296219f72fd0e0b8859ce66b43a9e92859af597
chris_bombilla <christian@bombilladigital.com>
2022-02-18 12:50:32 -0600

    modificacno apariencia de los emails options

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<div class="container mx-auto">
+    <div class="flex flex-wrap justify-center">
+        <div class="w-full max-w-sm">
+            <div class="flex flex-col break-words bg-white border-2 shadow-md mt-20">
+                <div class="bg-gray-300 text-gray-800 uppercase text-center py-3 px-6 mb-0">
+                    {{ __('Reset Password') }}
                 </div>
+                <form class="py-10 px-5" method="POST" action="{{ route('password.email') }}" novalidate>
+                    @csrf
+                    @if (session('status'))
+                    <span class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 w-full my-5 block text-sm"
+                        role="alert">
+                        <strong>{{ session('status') }}</strong>
+                    </span>
+                    @endif
+                    <div class="flex flex-wrap mb-6">
+                        <label for="email" class="block text-gray-700 text-sm mb-2">{{ __('E-Mail Address') }}</label>
+                        <input id="email" type="email"
+                            class="p-3 bg-gray-200 rounded form-input w-full focus:outline-none @error('email') border-red-500 border @enderror"
+                            name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
+
+                        @error('email')
+                        <span class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 w-full mt-5 text-sm"
+                            role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+
+                    </div>
+
+                    <div class="flex flex-wrap">
+                        <button type="submit" class="bg-teal-300 hover:bg-teal-700 shadow-md text-white-800
+                                 hover:text-white text-sm uppercase
+                                 mb-2 w-full py-3 rounded
+                                 focus:outline-none">
+                            {{ __('Send Password Reset Link') }}
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
