fa1e3d11ee6d663bd6ba011310f2f1d376b9e238
chris_bombilla <christian@bombilladigital.com>
2022-02-21 13:28:40 -0600

    Trayendo informacion de la BD al front

--- resources/views/vacantes/create.blade.php
+++ resources/views/vacantes/create.blade.php
@@ -9,14 +9,23 @@
     <form class="max-w-lg mx-auto my-10" action="">
         <div class="mb-5">
             <label for="titulo" class="block text-gray-700 text-sm mb-2">Titulo Vacante: </label>
-            <input id="titulo" type="text" class="p-3 bg-white rounded form-input w-full focus:outline-none mb-3 
+            <input id="titulo" type="text" class="p-3 bg-white rounded form-input w-full focus:outline-none 
                                                   @error('password') is-invalid @enderror"
-            name="titulo">
-            {{ $categorias }}
-            <button type="submit" class="bg-teal-500 w-full hover:bg-teal-600 text-gray-100 font-bold p-3 focus:outline focus:shadow-outline uppercase">
-                Publicar vacante
-            </button>
-            
+            name="titulo">    
         </div>
+        <div class="mb-5">
+            <label for="categoria" class="block text-gray-700 text-sm mb-2">Categoria: </label>
+            <select class="block appearance-none w-full border border-gray-200 text-gray-700 rounded leading-tight focus:outline-none focus:border-gray-500 p-3 bg-white" id="categoria" name="categoria">
+                <option disabled selected>Selecciona tu categoria</option>
+                @foreach ($categorias as $categoria)
+                    <option value="{{ $categoria->id }}">
+                        {{ $categoria->nombre }}
+                    </option>
+                @endforeach
+            </select>
+        </div>
+        <button type="submit" class="bg-teal-500 w-full hover:bg-teal-600 text-gray-100 font-bold p-3 focus:outline focus:shadow-outline uppercase">
+            Publicar vacante
+        </button>
     </form>
 @endsection
