cc927258947899fdf2ee3b2ad38dbad1c10475dd
chris_bombilla <christian@bombilladigital.com>
2022-02-21 13:17:09 -0600

    Creacion del seed para las categorias

--- resources/views/vacantes/create.blade.php
+++ resources/views/vacantes/create.blade.php
@@ -6,4 +6,17 @@
 
 @section('content')
     <h1 class="text-2xl text-center mt-10">Nueva vacante</h1>
+    <form class="max-w-lg mx-auto my-10" action="">
+        <div class="mb-5">
+            <label for="titulo" class="block text-gray-700 text-sm mb-2">Titulo Vacante: </label>
+            <input id="titulo" type="text" class="p-3 bg-white rounded form-input w-full focus:outline-none mb-3 
+                                                  @error('password') is-invalid @enderror"
+            name="titulo">
+            {{ $categorias }}
+            <button type="submit" class="bg-teal-500 w-full hover:bg-teal-600 text-gray-100 font-bold p-3 focus:outline focus:shadow-outline uppercase">
+                Publicar vacante
+            </button>
+            
+        </div>
+    </form>
 @endsection
